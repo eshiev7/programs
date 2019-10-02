777ea7d452a1ef88dc68a688565bab42a7bec17a
eshiev7 <39335526+eshiev7@users.noreply.github.com>
2019-10-02 14:58:46 +0700

    Update Circle.java

--- Geometry/Circle.java
+++ Geometry/Circle.java
@@ -1,34 +1,48 @@
 package ru.nsu.home;
 
-public class Circle extends Figure{
+public class Rectangle extends Figure {
 
-    private int r;
     private int x1;
+    private int x2;
     private int y1;
+    private int y2;
+    private int a;
+    private int b;
 
-    public  Circle(int r, int x1, int y1){
-        this.r = r;
+    public Rectangle(int x1, int y1, int x2, int y2){
         this.x1 = x1;
         this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.a = Math.abs(this.x2 - this.x1);
+        this.b = Math.abs(this.y2 - this.y1);
     }
-    //---------------Area-------------------------
+//---------------Area-------------------------
     public double getArea(){
-        return Math.PI * Math.pow(this.r, 2);
+        return this.a * this.b;
     }
-    //---------------Movement----------------------
+//---------------Movement----------------------
     public void changeX(int x){
         this.x1 += x;
+        this.x2 += x;
     }
 
     public void changeY(int y){
         this.y1 += y;
+        this.y2 += y;
     }
-    //---------------ChangeSize---------------------
-    public void size(int r){
-        this.r += r;
+//---------------ChangeSize---------------------
+    public void size(int a, int b){
+        this.x2 += a;
+        this.y2 += b;
+        this.a = Math.abs(this.x2 - this.x1);
+        this.b = Math.abs(this.y2 - this.y1);
     }
-    //---------------Getter-------------------------
+//---------------Getter-------------------------
     public String getInfo(){
-        return "X1: " + this.x1 + ", " + "Y1: " + this.y1 + ", " + "Radius: " + this.r;
+         return "X1: " + this.x1 + ", " +
+                 "Y1: " + this.y1 + ", " +
+                 "X2: " + this.x2 + ", " +
+                 "Y2: " + this.y2;
     }
 }
