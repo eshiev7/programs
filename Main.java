f0ce2c14377b8ebde8b3d954bf207e0cda777b9a
eshiev7 <39335526+eshiev7@users.noreply.github.com>
2019-10-02 13:56:37 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,39 @@
-package com.ksu.ru;
+package ru.nsu.home;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        ComplexNumbers kek1 = new ComplexNumbers(4,4);
-        System.out.println(kek1.getA());
-        System.out.println(kek1.getB());
-        System.out.println(kek1.getAll());
-        kek1.increase(8,3);
-        System.out.println(kek1.getAll());
-        kek1.reduce(7,25);
-        System.out.println(kek1.getAll());
-        kek1.multiply(4,-5);
-        System.out.println(kek1.getAll());
+	    Rectangle kek1 = new Rectangle(3, 2, 6, 4);
+        System.out.println(kek1.getArea());
+        System.out.println(kek1.getInfo());
+        kek1.changeX(4);
+        System.out.println(kek1.getInfo());
+        System.out.println(kek1.getArea());
+        kek1.size(1,2);
+        System.out.println(kek1.getInfo());
+        System.out.println(kek1.getArea());
+        System.out.println("-----------------------------------------");
+        Square kek2 = new Square(2,3, 2);
+        System.out.println(kek2.getArea());
+        System.out.println(kek2.getInfo());
+        kek2.changeX(4);
+        System.out.println(kek2.getInfo());
+        System.out.println(kek2.getArea());
+        kek2.size(1);
+        System.out.println(kek2.getInfo());
+        System.out.println(kek2.getArea());
+        System.out.println("-----------------------------------------");
+        Circle kek3 = new Circle(5,3, 2);
+        System.out.println(kek3.getArea());
+        System.out.println(kek3.getInfo());
+        kek3.changeX(4);
+        System.out.println(kek3.getInfo());
+        System.out.println(kek3.getArea());
+        kek3.size(1);
+        System.out.println(kek3.getInfo());
+        System.out.println(kek3.getArea());
     }
 }
