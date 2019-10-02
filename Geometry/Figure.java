d0895e03112f504497ff3c2845c770dd226678fe
eshiev7 <39335526+eshiev7@users.noreply.github.com>
2019-10-02 14:57:32 +0700

    Update Figure.java

--- Geometry/Figure.java
+++ Geometry/Figure.java
@@ -2,4 +2,7 @@ package ru.nsu.home;
 
 public abstract class Figure {
     abstract double getArea();
+    abstract String getInfo();
+    abstract void changeX(int x);
+    abstract void changeY(int y);
 }
