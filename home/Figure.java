d53b695197762a039e467bdef2d03d7c551fd7dd
eshiev7 <39335526+eshiev7@users.noreply.github.com>
2019-10-02 15:14:45 +0700

    Add files via upload

---
+++ home/Figure.java
@@ -0,0 +1,9 @@
+package ru.nsu.home;
+
+public abstract class Figure {
+    abstract double getArea();
+    abstract String getInfo();
+    abstract void changeX(int x);
+    abstract void changeY(int y);
+    abstract String getName();
+}
